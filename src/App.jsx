669e008c5e3fa24105872cb8e86ebf428ebaac40
Vinicius Santos <santosviniv@gmail.com>
2022-05-19 12:34:36 -0300

    new: wip status in the site

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import WIP from './components/WIP/WorkInProgress';
+
+function App() {
+  return (
+    <div>
+      <div className="main">
+        <WIP />
+      </div>
+    </div>
+  );
+}
+
+export default App;
